7f31415a1ef8e082063640c2d184eb822fe05520
amineohn <amineprojet7@gmail.com>
2022-11-03 15:58:47 +0100

    chore: more things

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 export default function Home() {
   const crosshairs = [
     {
+      id: 1,
       config: "",
       color: "red",
+    },
+    {
+      id: 2,
+      config: "",
+      color: "green",
     },
   ];
   //https://www.serilineups.com/crosshairlar.html
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <main className="flex min-h-screen flex-col place-content-center space-y-6">
-        <section className="grid grid-cols-5 gap-2">
+        <section className="grid grid-cols-8 gap-2">
           {crosshairs.map((item) => (
             <>
-              {[...Array(10)].map((_, i) => (
-                <div className={`h-10 w-10 rounded-full bg-blue-500`} />
+              {[...Array(20)].map((_, i) => (
+                <div className="h-10 w-10 rounded-full bg-blue-500 px-2.5 py-2 text-white">
+                  {i}
+                </div>
               ))}
 
               <p className="text-center text-gray-800">{item.config}</p>
