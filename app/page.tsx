8d14b6705f43aa987b8970a80df3ed3621a66183
amineohn <amineprojet7@gmail.com>
2022-11-03 13:15:16 +0100

    wip: nav + page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,23 @@
-import Button from "./components/button";
-
 export default function Home() {
+  const crosshair = [
+    {
+      config: "",
+      color: "red",
+    },
+  ];
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <main className="flex min-h-screen flex-col place-content-center space-y-6">
-        <section className="flex flex-col space-y-4 rounded-md p-10 text-center">
-          <h1 className="text-6xl font-extrabold text-neutral-900">
-            Welcome to{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://nextjs.org"
-            >
-              Next.js 13!
-            </a>
-          </h1>
-          <p className="text-2xl text-neutral-600">
-            Get started by editing{" "}
-            <code className="rounded-md bg-neutral-100 p-3 font-mono text-lg">
-              app/page.tsx
-            </code>
-          </p>
-          <Button label="Go to About" route="/about" />
+        <section className="grid grid-cols-5 gap-2">
+          {crosshair.map((item) => (
+            <>
+              {[...Array(10)].map((_, i) => (
+                <div className={`h-10 w-10 rounded-full bg-blue-500`} />
+              ))}
+
+              <p className="text-center text-gray-800">{item.config}</p>
+            </>
+          ))}
         </section>
       </main>
     </div>
