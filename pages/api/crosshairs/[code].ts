eeba33b2b40d1717903127d23d257c93557c9030
amineohn <amineprojet7@gmail.com>
2023-01-30 13:46:40 +0100

    feat: upload features imageKit

---
+++ pages/api/crosshairs/[code].ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import api from "unofficial-valorant-api";
+import ImageKit from "imagekit";
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const valorant = new api();
+  const { code } = req.query;
+  const imagekit = new ImageKit({
+    publicKey: "public_K3IgvAAA8n1QIaxB3jBEHYQlJJ4=",
+    privateKey: "private_n5y6fsGoS3eJ9L0DDfUP4Ws7KtQ=",
+    urlEndpoint: "https://ik.imagekit.io/gcfptzuc7",
+  });
+  const crosshairName = Math.random().toString(36).substring(7);
+
+  if (code) {
+    valorant.getCrosshair({ code: code.toString(), size: 512 }).then((data) => {
+      imagekit
+        .upload({
+          file: data.data as any,
+          fileName: `${crosshairName}.jpeg`,
+        })
+        .then((response) => {
+          res.status(200).json({
+            image: response.url,
+          });
+        });
+    });
+  }
+}
