e9ff15e08ab7d72dd3d2a49c0a029a9598aea927
amineohn <amineprojet7@gmail.com>
2023-01-30 13:53:52 +0100

    feat: add collection crosshairs, fix: set data as Buffer

--- pages/api/crosshairs/[code].ts
+++ pages/api/crosshairs/[code].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import api from "unofficial-valorant-api";
 import ImageKit from "imagekit";
+import { Firebase } from "../../../services/firebase.service";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const valorant = new api();
   const { code } = req.query;
@@ -10,15 +11,25 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     urlEndpoint: "https://ik.imagekit.io/gcfptzuc7",
   });
   const crosshairName = Math.random().toString(36).substring(7);
-
+  const firebase = new Firebase();
   if (code) {
     valorant.getCrosshair({ code: code.toString(), size: 512 }).then((data) => {
       imagekit
         .upload({
-          file: data.data as any,
+          file: data.data as Buffer,
           fileName: `${crosshairName}.jpeg`,
         })
         .then((response) => {
+          const date = new Date();
+          const dateString = `${date.getDate()}-${
+            date.getMonth() + 1
+          }-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+          firebase.collection("crosshairs").add({
+            name: crosshairName,
+            code: code.toString(),
+            url: response.url,
+            createdAt: dateString,
+          });
           res.status(200).json({
             image: response.url,
           });
